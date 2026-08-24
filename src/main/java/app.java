abd0ef464625881a8498bdf0a47e5c0fb11f7070
joshitha <n.joshitha246@gmail.com>
2026-08-24 10:00:40 +0530

    Initial StudentApp project

---
+++ src/main/java/app.java
@@ -0,0 +1,7 @@
+public class app {
+
+    public static void main(String[] args) {
+        System.out.println("Hello from StudentApp!");
+        System.out.println("CI Pipeline is working!");
+    }
+}
